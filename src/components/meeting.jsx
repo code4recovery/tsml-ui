07a1184493441c03adf9f55e2387a0d52a41b137
Josh Reisner <josh@joshreisner.com>
2018-11-15 07:56:43 -0800

    inside page

--- src/components/meeting.jsx
+++ src/components/meeting.jsx
@@ -31,29 +31,31 @@ export default class Meeting extends Component {
 			<div className={classNames('row', {
 				'd-none': !this.props.state.input.meeting
 			})}>
-				<div className="col offset-md-1 col-md-10 py-3">
-					<h1 className="font-weight-light pb-2">
+				<div className="col py-3">
+					<h1 className="font-weight-light">
 						<a href={window.location.pathname} onClick={event=>this.goBack(event)}>{settings.strings.meetings}</a>
 						<span className="mx-1">&rarr;</span>
 						{meeting.name}
 					</h1>
 					<div className="row">
-						<div className="col col-md-4">
+						<div className={classNames('col', {'col-md-4': this.props.state.capabilities.map})}>
 							<a className="btn btn-outline-secondary btn-block mb-3" href="">Get Directions</a>
 							<ul className="list-group">
 								<li className="list-group-item">
 									<h5>Meeting Information</h5>
 									<p className="mb-1">{settings.strings[settings.days[meeting.day]]}, {meeting.time_formatted}</p>
-									<ul>
-										<li>Discussion</li>
-										<li>Meditation</li>
-										<li>Speaker</li>
+									<ul className={{ 'd-none': !meeting.types || !meeting.types.length }}>
+										{meeting.types ? meeting.types.map(type => {
+											return(
+												<li key={type}>{settings.strings.types[type]}</li>
+											);
+										}) : ''}
 									</ul>
 								</li>
 								<li className="list-group-item">
 									<h5>{meeting.location}</h5>
 									<p className="mb-1">{meeting.formatted_address}</p>
-									<p className="mb-1">Also at this location:</p>
+									<p className="mb-1">Other meetings at this address:</p>
 									<ol>
 										<li>One Day at a Time</li>
 										<li>One Day at a Time</li>
@@ -63,8 +65,8 @@ export default class Meeting extends Component {
 								</li>
 							</ul>
 						</div>
-						<div className="col col-md-8">
-							<div className="border rounded bg-light h-100"></div>
+						<div className={classNames('col col-md-8', {'d-none': !this.props.state.capabilities.map})}>
+							<div className="border rounded bg-light h-100 map"></div>
 						</div>
 					</div>
 				</div>
