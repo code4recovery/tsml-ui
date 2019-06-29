9c3001f4cc0f03664eec9b5550dd029684ce1713
Jacob Elstein <contact@jacobelstein.com>
2019-06-29 16:47:26 -0400

    fixed some format time edge cases

--- src/helpers/format-time.jsx
+++ src/helpers/format-time.jsx
@@ -6,9 +6,10 @@ export default function FormatTime(time) {
 	hours = parseInt(hours);
 	if (hours == 0) {
 		hours = 12;
+		if (minutes == '00') return 'Mid';
 	} else if (hours > 11) {
 		if (hours == 12 && minutes == '00') return 'Noon'; 
-		if (hours == 23 && minutes == '59') return 'Mid';
+		if (hours == 23 && minutes == '59' || hours == 24 && minutes == '00') return 'Mid';
 		ampm = 'pm';
 		if (hours > 12) hours -= 12;
 	}
