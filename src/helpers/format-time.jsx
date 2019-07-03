09d62b7e02c420c2d4ce2779b3f96a12b558e614
Josh Reisner <josh@joshreisner.com>
2019-07-03 07:56:15 -0500

    adding @mathteacher's code change to googlesheet, and my refactor of format-time to replace #16

--- src/helpers/format-time.jsx
+++ src/helpers/format-time.jsx
@@ -1,16 +1,23 @@
+import { strings } from '../settings';
+
 //quick format time function
 export default function FormatTime(time) {
 	if (time == null || time.length == 0) return null;
-	let [ hours, minutes ] = time.split(':');
-	let ampm = 'am';
-	hours = parseInt(hours);
-	if (hours == 0) {
-		hours = 12;
-	} else if (hours > 11) {
-		if (hours == 12 && minutes == '00') return 'Noon'; 
-		if (hours == 23 && minutes == '59') return 'Mid';
-		ampm = 'pm';
-		if (hours > 12) hours -= 12;
-	}
-	return hours + ':' + minutes + ' ' + ampm;
+
+	let date = new Date();
+	const [ hours, minutes ] = time.split(':');
+
+	if (hours == '12' && minutes == '00') return strings.noon;
+	if (
+		(hours == '00' && minutes == '00') || 
+		(hours == '23' && minutes == '59') || 
+		(hours == '24' && minutes == '00')
+	) return strings.midnight;
+
+	date.setHours(parseInt(hours));
+	date.setMinutes(parseInt(minutes));
+	return date.toLocaleTimeString([], {
+		hour: '2-digit',
+		minute:'2-digit'
+	});
 }
