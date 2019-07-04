01c77e584b53b5c26b96b77f56ca2be50db1e91a
Josh Reisner <josh@joshreisner.com>
2019-07-03 19:05:27 -0500

    adding comments, deferring date creation

--- src/helpers/format-time.jsx
+++ src/helpers/format-time.jsx
@@ -2,18 +2,26 @@ import { strings } from '../settings';
 
 //quick format time function
 export default function FormatTime(time) {
-	if (time == null || time.length == 0) return null;
 
-	let date = new Date();
+	//check that string is HH:MM
+	if (!time || time.length !== 5 || time.substr(2, 1) !== ':') return null;
+
+	//get hours and minutes
 	const [ hours, minutes ] = time.split(':');
 
-	if (hours == '12' && minutes == '00') return strings.noon;
-	if (
+	//check for times with special names
+	if (hours == '12' && minutes == '00') {
+		return strings.noon;
+	} else if (
 		(hours == '00' && minutes == '00') || 
 		(hours == '23' && minutes == '59') || 
 		(hours == '24' && minutes == '00')
-	) return strings.midnight;
+	) {
+		return strings.midnight;
+	}
 
+	//create a date object
+	let date = new Date();
 	date.setHours(parseInt(hours));
 	date.setMinutes(parseInt(minutes));
 	return date.toLocaleTimeString([], {
