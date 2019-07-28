a8e24b7d1c0bdbb0b3c49d386b9386339b074af9
Josh Reisner <josh@joshreisner.com>
2019-07-27 20:12:25 -0400

    fixing double-latitude-check to fix #25

--- src/helpers/load-data.jsx
+++ src/helpers/load-data.jsx
@@ -176,7 +176,7 @@ export default function LoadData(meetings, capabilities) {
     }
 
     //build index of map pins
-    if (meeting.latitude && meeting.latitude) {
+    if (meeting.latitude && meeting.longitude) {
       capabilities.coordinates = true;
     }
 
