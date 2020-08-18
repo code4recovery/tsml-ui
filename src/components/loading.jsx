9a548b2a5463e4aac4ebaaa8c36e9f497306bc8e
Josh Reisner <jreisner@atlassian.com>
2020-08-18 07:05:47 -0700

    working on component cases

--- src/components/loading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Loading() {
-  return (
-    <div className="d-flex h-100 flex-grow-1 align-items-center justify-content-center loading">
-      <div className="spinner-border text-secondary m-5" />
-    </div>
-  );
-}
