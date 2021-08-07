150b0ae176f5114ee40841e4c89fd80be7e43a6a
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2021-08-07 08:36:47 -0700

    switching values to labels for #139

--- src/helpers/analytics.js
+++ src/helpers/analytics.js
@@ -1,21 +1,14 @@
 //send analytics to google (other providers possible)
-//category = 'search', action = 'search' or 'location', label = 'search_term', value = 'odaat'
-export function analyticsEvent({
-  category,
-  action,
-  label,
-  value,
-  debug = false,
-}) {
+//category = 'search', action = 'search' or 'location', label = 'odaat'
+export function analyticsEvent({ category, action, label, debug = false }) {
   if (typeof gtag === 'function') {
     //https://developers.google.com/analytics/devguides/collection/gtagjs/events
     gtag('event', action, {
       event_category: category,
       event_label: label,
-      value: value,
     });
     if (debug) {
-      console.log(`recorded gtag event for "${value}"`);
+      console.log(`recorded gtag event for "${label}"`);
     }
   } else if (typeof ga === 'function') {
     //https://developers.google.com/analytics/devguides/collection/analyticsjs/events
@@ -24,10 +17,9 @@ export function analyticsEvent({
       eventCategory: category,
       eventAction: action,
       eventLabel: label,
-      eventValue: value,
     });
     if (debug) {
-      console.log(`recorded ga event for "${value}"`);
+      console.log(`recorded ga event for "${label}"`);
     }
   } else if (debug) {
     console.log('did not record analytics event');
