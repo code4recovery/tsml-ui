1da3171d170d5498e9d06b77be07a3780098f963
Chris Sobeck (cigzigwon) <factchkr@gmail.com>
2021-08-30 21:15:17 -0400

    Fix issue with CSS hook internal usage because mix.sass is deferred preprocessor

--- src/components/TsmlUI.jsx
+++ src/components/TsmlUI.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-import '../../public/style.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import { Alert, Controls, Loading, Map, Meeting, Table, Title } from './';
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -p 3000:3000 -v $(pwd):/usr/src/apps --rm cigzigwon/yarn:fermium-alpine yarn hot
+docker run -it -p 3000:3000 -v $(pwd):/usr/src/apps --rm cigzigwon/yarn:fermium-alpine yarn dev
