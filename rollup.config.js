0085a0557e9aa8327c5562576358b975c1ef3a14
Chris Sobeck (cigzigwon) <factchkr@gmail.com>
2021-08-31 12:39:37 -0400

    Add app packaging for NPM

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "rollup-plugin-commonjs";
 import resolve from "rollup-plugin-node-resolve";
 import external from "rollup-plugin-peer-deps-external";
 import sass from 'rollup-plugin-sass';
+import { terser } from "rollup-plugin-terser";
 import { uglify } from "rollup-plugin-uglify";
 import css from 'rollup-plugin-css-only';
 import json from '@rollup/plugin-json';
@@ -10,6 +11,11 @@ import json from '@rollup/plugin-json';
 import packageJSON from "./package.json";
 const input = "./src/index.js";
 const minifyExtension = pathToFile => pathToFile.replace(/\.js$/, ".min.js");
+const common_cfg = {
+  namedExports: {
+    'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'useEffect', 'useRef', 'useState', 'createContext', 'useContext', 'useLayoutEffect', 'useCallback', 'useImperativeHandle', 'forwardRef', 'useMemo', 'cloneElement', 'createRef']
+  }
+}
 
 export default [
   // CommonJS
@@ -25,11 +31,7 @@ export default [
       }),
       external(),
       resolve(),
-      commonjs({
-        namedExports: {
-          'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'useEffect', 'useRef', 'useState', 'createContext', 'useContext', 'useLayoutEffect', 'useCallback', 'useImperativeHandle', 'forwardRef', 'useMemo', 'cloneElement', 'createRef']
-        }
-      }),
+      commonjs(common_cfg),
       sass({ output: true }),
       css({ output: 'bundle.css' }),
       json()
@@ -47,15 +49,99 @@ export default [
       }),
       external(),
       resolve(),
-      commonjs({
-        namedExports: {
-          'node_modules/react/index.js': ['Component', 'PureComponent', 'Fragment', 'Children', 'createElement', 'useEffect', 'useRef', 'useState', 'createContext', 'useContext', 'useLayoutEffect', 'useCallback', 'useImperativeHandle', 'forwardRef', 'useMemo', 'cloneElement', 'createRef']
-        }
-      }),
+      commonjs(common_cfg),
       sass({ output: true }),
       css({ output: 'bundle.css' }),
       json(),
       uglify()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: packageJSON.browser,
+      format: "umd",
+      name: "tsmlUI",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(common_cfg),
+      sass({ output: true }),
+      css({ output: 'bundle.css' }),
+      json()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.browser),
+      format: "umd",
+      name: "tsmlUI",
+      globals: {
+        react: "React",
+        "@emotion/styled": "styled",
+        "@emotion/core": "core"
+      }
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(common_cfg),
+      sass({ output: true }),
+      css({ output: 'bundle.css' }),
+      json(),
+      terser()
+    ]
+  },
+  {
+    input,
+    output: {
+      file: packageJSON.module,
+      format: "es",
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(common_cfg),
+      sass({ output: true }),
+      css({ output: 'bundle.css' }),
+      json(),
+    ]
+  },
+  {
+    input,
+    output: {
+      file: minifyExtension(packageJSON.module),
+      format: "es",
+      exports: "named"
+    },
+    plugins: [
+      babel({
+        exclude: "node_modules/**"
+      }),
+      external(),
+      resolve(),
+      commonjs(common_cfg),
+      sass({ output: true }),
+      css({ output: 'bundle.css' }),
+      json(),
+      terser()
     ]
   }
 ];
