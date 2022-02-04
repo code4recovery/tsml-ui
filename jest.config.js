936951cd9e167ce11ef9bb0f92dfb1364a00510f
Chris <chris@altruisticsoftware.com>
2022-02-03 17:59:32 -0700

    Add tests (#188)
    * Add tests
    
    * Rebase and fix test
    
    * Get coverage from action
    
    * Remove coveralls dep

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import("@jest/types").Config.InitialOptions} */
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.(j|t)s*',
+    '!<rootDir>/src/(types|i18n)/*',
+  ],
+};
