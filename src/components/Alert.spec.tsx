1f5171c2b35058b881ef1682ffc6b2809e027327
Chris <chris@altruisticsoftware.com>
2022-02-07 06:25:21 -0700

    Implement react testing library (#193)
    * Implement react testing library
    
    * Fix lockfile
    
    * Remove react import

---
+++ src/components/Alert.spec.tsx
@@ -0,0 +1,82 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Alert from './Alert';
+
+//TODO: These types can be much better once AppState types are defined.
+
+describe('<Alert />', () => {
+  it('renders null with no alerts or errors', () => {
+    const { container } = render(<Alert state={{}} setState={jest.fn()} />);
+    expect(container.firstChild).toBeNull();
+  });
+
+  it('works with error state', () => {
+    render(<Alert state={{ error: 'bad_data' }} setState={jest.fn()} />);
+
+    const reloadSpy = jest.spyOn(location, 'reload');
+
+    const text = /an error was encountered loading the data/i;
+    const button = screen.getByText(/reload/i);
+
+    expect(screen.getByText(text)).toBeInTheDocument();
+    fireEvent.click(button);
+    expect(reloadSpy).toHaveBeenCalled();
+  });
+
+  it('works with clearing filters with no results', async () => {
+    const mockState = {
+      alert: 'no_results',
+      input: {
+        distance: [],
+        region: ['foo'],
+        search: 'bar',
+        time: ['baz'],
+        type: ['qux'],
+        weekday: ['0'],
+      },
+      indexes: {
+        region: [{ key: 'foo', name: 'Foo' }],
+        time: [{ key: 'baz', name: 'Baz' }],
+        type: [{ key: 'qux', name: 'Qux' }],
+        weekday: [{ key: '0', name: 'Monday' }],
+      },
+    };
+
+    const mockSetState = jest.fn();
+
+    render(<Alert state={mockState} setState={mockSetState} />);
+
+    const text = /No meetings were found matching the selected criteria./i;
+    expect(screen.getByText(text)).toBeInTheDocument();
+
+    const removeSearchButton = screen.getByText(/remove ‘bar’/i);
+    const removeRegionButton = screen.getByText(/remove foo/i);
+    const removeTimeButton = screen.getByText(/remove baz/i);
+    const removeTypeButton = screen.getByText(/remove qux/i);
+    const removeWeekdayButton = screen.getByText(/remove monday/i);
+
+    function modify<
+      K extends keyof typeof mockState['input'],
+      T extends typeof mockState['input'][K]
+    >(key: K, value: T) {
+      return {
+        ...mockState,
+        input: { ...mockState.input, [key]: value },
+      };
+    }
+
+    fireEvent.click(removeSearchButton);
+    expect(mockSetState).toHaveBeenLastCalledWith(modify('search', ''));
+
+    fireEvent.click(removeRegionButton);
+    expect(mockSetState).toHaveBeenLastCalledWith(modify('region', []));
+
+    fireEvent.click(removeTimeButton);
+    expect(mockSetState).toHaveBeenLastCalledWith(modify('time', []));
+
+    fireEvent.click(removeTypeButton);
+    expect(mockSetState).toHaveBeenLastCalledWith(modify('type', []));
+
+    fireEvent.click(removeWeekdayButton);
+    expect(mockSetState).toHaveBeenLastCalledWith(modify('weekday', []));
+  });
+});
