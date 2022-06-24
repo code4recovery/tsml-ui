9d1e0ec691605af04012d619b3129dddcbf03975
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2022-06-24 07:04:03 -0700

    refactor ical format for #234

--- src/helpers/format/format-ics.ts
+++ src/helpers/format/format-ics.ts
@@ -4,82 +4,81 @@ import { Meeting } from '../../types';
 export function formatIcs(meeting: Meeting) {
   const fmt = 'YYYYMMDDTHHmmss';
 
-  const url = [
-    'BEGIN:VCALENDAR',
-    'VERSION:2.0',
-    'BEGIN:VEVENT',
+  //need an end time. guess one hour if none specified
+  if (!meeting.end) {
+    meeting.end = meeting.start.clone().add(1, 'hour');
+  }
+
+  //start building event
+  const event = [
     `SUMMARY:${meeting.name}`,
     `DTSTART:${meeting.start.clone().tz('UTC').format(fmt)}Z`,
     `DTSTART;TZID=/${meeting.timezone}:${meeting.start.format(fmt)}`,
+    `DTEND:${meeting.end.clone().tz('UTC').format(fmt)}Z`,
+    `DTEND;TZID=/${meeting.timezone}:${meeting.end.format(fmt)}`,
+  ];
+
+  //start building description
+  const description = [];
+
+  //add notes
+  if (meeting.notes) {
+    description.push(meeting.notes);
+  }
+
+  //add in-person info
+  if (meeting.isInPerson) {
+    event.push(`LOCATION:${meeting.location}\n${meeting.formatted_address}`);
+    if (meeting.location_notes) {
+      description.push(meeting.location_notes);
+    }
+  }
+
+  //add online info
+  if (meeting.isOnline) {
+    if (meeting.conference_provider) {
+      if (meeting.conference_url_notes) {
+        description.push(meeting.conference_url_notes);
+      }
+      description.push(
+        '----( Video Call )----',
+        meeting.conference_url,
+        '---===---'
+      );
+    }
+    if (meeting.conference_phone) {
+      description.push(meeting.conference_phone);
+      if (meeting.conference_phone_notes) {
+        description.push(meeting.conference_phone_notes);
+      }
+    }
+  }
+
+  if (description.length) {
+    event.push(`DESCRIPTION:${description.join('\n')}`);
+  }
+
+  //add group website
+  if (meeting.website) {
+    event.push(`URL:${meeting.website}`);
+  }
+
+  //format event string
+  const blob = [
+    'BEGIN:VCALENDAR',
+    'VERSION:2.0',
+    'BEGIN:VEVENT',
+    ...event.map(line => line.replaceAll('\n', '\\n').replaceAll(',', '\\,')),
     'END:VEVENT',
     'END:VCALENDAR',
-  ];
+  ].join('\n');
 
-  if (meeting.end) {
-    url.splice(
-      -2,
-      0,
-      `DTEND:${meeting.end.clone().tz('UTC').format(fmt)}Z`,
-      `DTEND;TZID=/${meeting.timezone}:${meeting.end.format(fmt)}`
-    );
-  } else {
-    url.splice(
-      -2,
-      0,
-      `DTEND:${meeting.start.clone().add(1, 'hour').tz('UTC').format(fmt)}Z`,
-      `DTEND;TZID=/${meeting.timezone}:${meeting.start
-        .clone()
-        .add(1, 'hour')
-        .format(fmt)}`
-    );
-  }
-
-  //start building notes
-  const notes = [
-    meeting.conference_url_notes,
-    meeting.conference_phone_notes,
-    meeting.notes,
-    meeting.location_notes,
-  ];
-
-  if (meeting.isInPerson) {
-    //address for in-person meetings
-    url.splice(
-      -2,
-      0,
-      `LOCATION:${meeting.formatted_address.replaceAll(',', ';')}`
-    );
-    if (meeting.location) {
-      notes.push(meeting.location);
-    }
-    if (meeting.latitude && meeting.longitude) {
-      url.splice(-2, 0, `GEO:${meeting.latitude};${meeting.longitude}`);
-    }
-  } else if (meeting.location) {
-    //location name
-    url.splice(-2, 0, `LOCATION:${meeting.location}`);
-  }
-
-  //add notes if present
-  const notesString = notes.filter(e => e).join('\\n');
-  if (notesString) {
-    url.splice(-2, 0, `DESCRIPTION:${notesString}`);
-  }
-
-  //add URL if present
-  if (meeting.conference_provider) {
-    url.splice(-2, 0, `URL:${meeting.conference_url.replaceAll('&amp;', '&')}`);
-  }
-
-  const urlString = url.join('\n');
-
-  if (/msie\s|trident\/|edge\//i.test(window.navigator.userAgent)) {
-    //open/save link in IE and Edge
-    const blob = new Blob([urlString], { type: 'text/calendar;charset=utf-8' });
-    window.navigator.msSaveBlob(blob, 'download.ics');
-  } else {
-    //open/save link in modern browsers
-    const uri = `data:text/calendar;charset=utf8,${urlString}`;
-    window.location = encodeURI(uri) as unknown as Location;
-  }
+  //create temporary link to download
+  const url = window.URL.createObjectURL(new Blob([blob]));
+  const link = document.createElement('a');
+  link.href = url;
+  link.setAttribute('download', `${meeting.name}.ics`);
+  document.body.appendChild(link);
+  link.click();
+  link.parentNode?.removeChild(link);
 }
