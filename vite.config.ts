97435619ea7ef12b7c65052218fece8a6c0f40bb
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2022-08-27 18:30:20 -0700

    build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,29 @@
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssInjectedByJsPlugin()],
+  publicDir: 'assets',
   resolve: {
     alias: {
       '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
     },
   },
   build: {
+    outDir: 'public',
+    target: 'es2015',
+    minify: 'esbuild',
+    assetsDir: '',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        aasanjose: resolve(__dirname, 'nested/aasanjose.html'),
+        app: resolve(__dirname, 'tests/aasanjose.html'),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        manualChunks: undefined,
       },
     },
   },
