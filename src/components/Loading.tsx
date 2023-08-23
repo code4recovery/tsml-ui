ffef8b547d89e0b748a2bc4ada2d1db6e7d18e59
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-08-22 22:34:11 -0700

    fix test height 100

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="align-items-center d-flex justify-content-center loading w-100">
+    <div className="align-items-center d-flex justify-content-center w-100">
       <div className="m-5 spinner-border text-secondary" />
     </div>
   );
