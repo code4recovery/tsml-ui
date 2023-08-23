661a095aa4b74c89eb99f374a05da79fd46b6cca
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-08-22 21:39:18 -0700

    refactor test css

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="align-items-center d-flex flex-grow-1 h-100 justify-content-center loading">
+    <div className="align-items-center d-flex justify-content-center loading w-100">
       <div className="m-5 spinner-border text-secondary" />
     </div>
   );
