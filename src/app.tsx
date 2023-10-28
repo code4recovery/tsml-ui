54497803ebdc3ca2f49410b8e53e97e936bac14c
Michael Duren <michaeld@michaelduren.com>
2023-10-28 08:00:06 -0500

    feature: install react-router-dom && setup router
    - Installed react-router-dom
    - Setup router in `app.tsx`
    - Tests are same result as previous (1 fails)
    - Manually tested routing, works the same.

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,39 @@
 import { createRoot } from 'react-dom/client';
 
 import { TsmlUI } from './components';
+import {
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
 // locate element
-const element = document.getElementById('tsml-ui');
+const el = document.getElementById('tsml-ui');
 
-if (element) {
-  createRoot(element).render(
-    <TsmlUI
-      google={element.getAttribute('data-google') || undefined}
-      mapbox={element.getAttribute('data-mapbox') || undefined}
-      settings={
-        typeof tsml_react_config === 'undefined' ? undefined : tsml_react_config
-      }
-      src={element.getAttribute('data-src') || undefined}
-      timezone={element.getAttribute('data-timezone') || undefined}
-    />
-  );
+export let routes: RouteObject[] = [];
+export let router: ReturnType<typeof createBrowserRouter>;
+
+if (el) {
+  routes = [
+    {
+      path: '/*',
+      element: (
+        <TsmlUI
+          google={el.getAttribute('data-google') || undefined}
+          mapbox={el.getAttribute('data-mapbox') || undefined}
+          settings={
+            typeof tsml_react_config === 'undefined'
+              ? undefined
+              : tsml_react_config
+          }
+          src={el.getAttribute('data-src') || undefined}
+          timezone={el.getAttribute('data-timezone') || undefined}
+        />
+      ),
+    },
+  ];
+  router = createBrowserRouter(routes);
+  createRoot(el).render(<RouterProvider router={router} />);
 } else {
   console.warn('TSML UI could not find a div#tsml-ui element');
 }
