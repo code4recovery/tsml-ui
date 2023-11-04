5de0872b47bda6b64b129274088fb742fb0462c1
Michael Duren <michaeld@michaelduren.com>
2023-11-04 14:09:06 -0500

    add react router, create browser router

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,31 @@
 import { createRoot } from 'react-dom/client';
-
 import { TsmlUI } from './components';
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // locate element
 const element = document.getElementById('tsml-ui');
 
+export let routes: RouteObject[] = [];
+export let router: ReturnType<typeof createBrowserRouter>;
+
 if (element) {
-  createRoot(element).render(
-    <TsmlUI
-      google={element.getAttribute('data-google') || undefined}
-      mapbox={element.getAttribute('data-mapbox') || undefined}
-      settings={
-        typeof tsml_react_config === 'undefined' ? undefined : tsml_react_config
-      }
-      src={element.getAttribute('data-src') || undefined}
-      timezone={element.getAttribute('data-timezone') || undefined}
-    />
-  );
+  routes = [
+    {
+      path: '/*',
+      element: (
+        <TsmlUI
+          google={element.getAttribute('data-google') || undefined}
+          mapbox={element.getAttribute('data-mapbox') || undefined}
+          settings={typeof tsml_react_config === 'undefined' ? undefined : tsml_react_config}
+          src={element.getAttribute('data-src') || undefined}
+          timezone={element.getAttribute('data-timezone') || undefined}
+        />
+      ),
+    },
+  ];
+
+  router = createBrowserRouter(routes);
+  createRoot(element).render(<RouterProvider router={router} />);
 } else {
   console.warn('TSML UI could not find a div#tsml-ui element');
 }
