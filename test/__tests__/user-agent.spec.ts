a9fb7f2fa406fa31d6161251953003bf2d2493dc
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-11-04 09:22:59 -0700

    fix, move tests

---
+++ test/__tests__/user-agent.spec.ts
@@ -0,0 +1,24 @@
+import { iOS } from '../../src/helpers/user-agent';
+
+let platformGetter: jest.SpyInstance<string, []>;
+let userAgentGetter: jest.SpyInstance<string, []>;
+
+describe('iOS', () => {
+  beforeEach(() => {
+    platformGetter = jest.spyOn(window.navigator, 'platform', 'get');
+    userAgentGetter = jest.spyOn(window.navigator, 'userAgent', 'get');
+  });
+
+  it('detects iOS', () => {
+    platformGetter.mockReturnValue('iPhone');
+    const isIOS = iOS();
+    expect(isIOS).toBe(true);
+  });
+
+  it('detects Mac', () => {
+    platformGetter.mockReturnValue('MacIntel');
+    userAgentGetter.mockReturnValue('Mac');
+    const isIOS = iOS();
+    expect(isIOS).toBe(false);
+  });
+});
