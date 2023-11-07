250cb2d7c0495d5e4ef0da0a209619eb05062ac8
Michael Duren <michaeld@michaelduren.com>
2023-11-07 15:55:34 -0600

    simplify browser router in

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { TsmlUI } from './components';
-import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
 // locate element
 const element = document.getElementById('tsml-ui');
@@ -9,22 +13,25 @@ export let routes: RouteObject[] = [];
 export let router: ReturnType<typeof createBrowserRouter>;
 
 if (element) {
-  routes = [
+  const router = createBrowserRouter([
     {
       path: '/*',
       element: (
         <TsmlUI
           google={element.getAttribute('data-google') || undefined}
           mapbox={element.getAttribute('data-mapbox') || undefined}
-          settings={typeof tsml_react_config === 'undefined' ? undefined : tsml_react_config}
+          settings={
+            typeof tsml_react_config === 'undefined'
+              ? undefined
+              : tsml_react_config
+          }
           src={element.getAttribute('data-src') || undefined}
           timezone={element.getAttribute('data-timezone') || undefined}
         />
       ),
     },
-  ];
+  ]);
 
-  router = createBrowserRouter(routes);
   createRoot(element).render(<RouterProvider router={router} />);
 } else {
   console.warn('TSML UI could not find a div#tsml-ui element');
