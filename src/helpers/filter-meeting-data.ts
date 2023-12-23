9e83b7161b2ba6740f3882d419a2c71bef4f8201
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2023-12-22 16:48:49 -0800

    fix timing, bump spec (#352)
    * fix timing, bump spec
    
    * add registry to see if that helps
    
    * reinstall npm packages with updated registry
    
    * package lock update

--- src/helpers/filter-meeting-data.ts
+++ src/helpers/filter-meeting-data.ts
@@ -100,15 +100,14 @@ export function filterMeetingData(
           .then(result => {
             if (result.features && result.features.length) {
               //re-render page with new params
-              calculateDistances(
-                filteredSlugs,
-                result.features[0].center[1],
-                result.features[0].center[0],
+              calculateDistances({
+                latitude: result.features[0].center[1],
+                longitude: result.features[0].center[0],
                 setState,
-                state,
                 settings,
-                strings
-              );
+                state,
+                strings,
+              });
             } else {
               //show error
             }
@@ -116,15 +115,14 @@ export function filterMeetingData(
       } else if (state.input.mode === 'me') {
         navigator.geolocation.getCurrentPosition(
           position => {
-            calculateDistances(
-              filteredSlugs,
-              position.coords.latitude,
-              position.coords.longitude,
+            calculateDistances({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
               setState,
-              state,
               settings,
-              strings
-            );
+              state,
+              strings,
+            });
           },
           error => {
             console.warn(`TSML UI geolocation error: ${error.message}`);
