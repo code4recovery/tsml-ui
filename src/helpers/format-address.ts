41cf5dbd1098a78ff94bb54495f9fd4a4ba19716
George Kovats <kovatsgeorge@gmail.com>
2024-02-10 13:26:07 -0500

    Core address fix just render string before first comma I assume it's always a string, right? TS, figure that's granted. Would cast if not.

--- src/helpers/format-address.ts
+++ src/helpers/format-address.ts
@@ -1,5 +1,4 @@
 //get address from formatted_address
 export function formatAddress(formatted_address = '') {
-  const address = formatted_address.split(', ');
-  return address.length > 3 ? address[0] : undefined;
+  return formatted_address.replace(/,.*/, '');
 }
