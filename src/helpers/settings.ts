4fb6143707afa9440f76e7c030f7f36eefc9c709
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2024-06-23 16:45:29 -0700

    portuguese 🇵🇹

--- src/helpers/settings.ts
+++ src/helpers/settings.ts
@@ -3,7 +3,7 @@ import { createContext, useContext } from 'react';
 import merge from 'deepmerge';
 import { Settings } from 'luxon';
 
-import { en, es, fr, ja, nl, sk, sv } from '../i18n';
+import { en, es, fr, ja, nl, pt, sk, sv } from '../i18n';
 
 // override these on your page with tsml_react_config
 export const defaults: TSMLReactConfig = {
@@ -79,6 +79,7 @@ export const defaults: TSMLReactConfig = {
     fr,
     ja,
     nl,
+    pt,
     sk,
     sv,
   },
