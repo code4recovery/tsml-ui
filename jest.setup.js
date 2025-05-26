0f0b5bda809c8e4f1a069f9d6fa3b1835ec238a3
Patrick Joyce <pjaudiomv@gmail.com>
2025-05-26 07:25:33 -0400

    fix: mock window.matchMedia for Jest tests

--- jest.setup.js
+++ jest.setup.js
@@ -10,6 +10,21 @@ if (!global.TextDecoder) {
   global.TextDecoder = TextDecoder;
 }
 
+// window.matchMedia doesn't exist in test environment
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 global.React = React;
 
 const savedLocation = window.location;
