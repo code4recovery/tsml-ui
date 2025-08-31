414cb4e66d2978dfbd062bd55754c0f621ad2d35
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2025-08-30 20:59:52 -0700

    add test

---
+++ test/__tests__/TsmlUi.spec.tsx
@@ -0,0 +1,43 @@
+import { act, render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+import TsmlUI from '../../src/components/TsmlUi';
+
+describe('TsmlUI', () => {
+  // mock fetch
+  beforeEach(() => {
+    // @ts-ignore
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve([
+            {
+              slug: 'test-title-1',
+              name: 'Test Name 1',
+              time: '08:00',
+              day: 0,
+              types: ['O', 'M', 'X'],
+            },
+            {
+              slug: 'test-title-2',
+              name: 'Test Name 2',
+              time: '09:00',
+              day: 1,
+              types: ['O', 'M', 'X'],
+            },
+          ]),
+      })
+    );
+  });
+
+  it('renders correctly', async () => {
+    const { container } = await act(() =>
+      render(
+        <MemoryRouter>
+          <TsmlUI src=" " />
+        </MemoryRouter>
+      )
+    );
+    expect(container).toBeTruthy();
+  });
+});
